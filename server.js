ef2e17e1e9ac438272429b6afe8a781ddcf4d38a
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-04 16:18:15 -0400

    cleanup initial react app

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
